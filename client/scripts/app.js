7c5a66bd0d34625eccb72f965be672fa3eb064ae
lylynguyen <lylynguyen812@gmail.com>
2015-12-07 19:04:57 -0800

    updated dropdown

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,22 @@
 // YOUR CODE HERE:
 var app = {
   server: 'https://api.parse.com/1/classes/chatterbox',
-  init: function(){},
+  chatRooms: {},
+  init: function(){
+    app.fetch();
+
+  },
   addMessage: function (){
     var username = window.location.search.slice(10);
-    var message = $('#newchats').val();
-    app.send(message);
+    var message = $('#newchat').val();
+    var room = $('#roomSelect').val();
+    var messageObj = {
+      username: username,
+      text: message,
+      roomname: room
+    }
+    console.log(messageObj);
+    app.send(messageObj);
     $('#newchats').val('');
   },
   send: function(message){ 
@@ -37,16 +48,28 @@ var app = {
       contentType: 'application/json',
       success: function (data) {
         for(var i = 0; i < data.results.length; i++){
-          
           var $chat = $('<div></div>');
           var currentChat = data.results[i];
           var chatTime = moment(currentChat.createdAt).format('MMM Do YYYY, h:mm:ss a');
           var escapeText = _.escape(currentChat.text);
+          var currentRoom = currentChat.roomname;
+          if(!app.chatRooms[currentRoom] && currentRoom !== undefined && currentRoom !== null){
+            app.chatRooms[currentRoom] = currentRoom; 
+          }
           $chat.html(currentChat.username+ " " + escapeText + " " + currentChat.roomname + " " + chatTime);
           $chat.appendTo($('#chats'));
         }
         console.log('chatterbox: Message get');
         console.log(data);
+        var $roomDropDown = $('#roomSelect');
+    
+        for(var key in app.chatRooms) {
+          var $option = $('<option></option>');
+          $option.html(app.chatRooms[key]);
+          $option.appendTo($roomDropDown)
+          //console.log(key);
+        }
+
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -58,6 +81,12 @@ var app = {
   clearMessages: function(){
     var $chat = $('#chats');
     $chat.html('');
+  },
+
+  addRoom: function(newRoom){
+    var roomname = $('#room').val();
+    app.send(roomname);
+    $('#newchats').val('');
   }
 
 };
@@ -68,7 +97,7 @@ var app = {
 $(document).ready(function(){
 
 app.init();
-app.fetch();
+//app.fetch();
 $('#submit-chat').on('click', app.addMessage)
 $('#clear-chat').on('click', app.clearMessages)
 
